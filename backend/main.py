b5fd0253c0cfbc23b003d9ca995090e4e8551afc
justusjb <jujo03h@icloud.com>
2023-10-28 16:54:28 -0700

    added basic database boilerplate to backend

--- backend/main.py
+++ backend/main.py
@@ -1,23 +1,24 @@
-from fastapi import FastAPI
-from sqlalchemy import create_engine, MetaData
-from databases import Database
+from fastapi import FastAPI, HTTPException
+from sqlalchemy import create_engine, Column, Integer, String, ForeignKey
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "postgresql+asyncpg://your_username:your_password@localhost/costco_db"
+DATABASE_URL = "postgresql://user:password@localhost:5432/mydatabase"
 
-database = Database(DATABASE_URL)
-metadata = MetaData()
+Base = declarative_base()
+engine = create_engine(DATABASE_URL)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+class Order(Base):
+    __tablename__ = "orders"
+
+    id = Column(Integer, primary_key=True, index=True)
+    product_name = Column(String, index=True)
+    user_id = Column(String)
+    quantity = Column(Integer)
+
+# Create the database tables
+Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
-
-@app.on_event("startup")
-async def startup():
-    await database.connect()
-
-@app.on_event("shutdown")
-async def shutdown():
-    await database.disconnect()
-
-@app.get("/")
-async def read_root():
-    return {"Hello": "World"}
 
