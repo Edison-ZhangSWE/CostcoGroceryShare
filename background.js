371cd57156182e70aeb62bc2ab23a9b40552f41f
Edison Zhang <edisonzhangsw@gmail.com>
2023-10-28 06:28:36 -0700

    Made the menu appear -> Shows Add to Queue

---
+++ background.js
@@ -0,0 +1,4 @@
+// Background Script: Create a new file named background.js. 
+// This script will contain the logic of your extension. 
+// For now, let's keep it empty. As you develop, you'll add more code here.
+
---
+++ popup.js
@@ -0,0 +1,3 @@
+document.getElementById('addToQueue').addEventListener('click', function() {
+    // Logic to add the current item to the queue will go here
+});
