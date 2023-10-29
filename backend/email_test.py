3c75b78c7473ee59eaa28abb14b24cbf6a4c6d1e
justusjb <jujo03h@icloud.com>
2023-10-29 03:11:43 -0700

    update email script

--- backend/email_test.py
+++ backend/email_test.py
@@ -2,6 +2,7 @@ import os
 from sendgrid import SendGridAPIClient
 from sendgrid.helpers.mail import Mail
 
+# Fetching the API key from a file
 with open('api_key.txt', 'r') as file:
     SENDGRID_API_KEY = file.readline().strip()
 
@@ -15,12 +16,15 @@ def send_order_emails(item_name, orders):
     # Create a list of recipient emails from the orders
     recipient_emails = [order['user_id'] for order in orders]
 
+    # Determine the email content using the new render_email_content function
+    email_content = render_email_content(item_name, orders)
+
     # Create the email content
     message = Mail(
         from_email="edisonzhang2@gmail.com",
         to_emails=recipient_emails,
         subject=f"Order Confirmation for {item_name}",
-        html_content=f"<strong>Order Details:</strong><br>{order_details}"
+        html_content=email_content
     )
 
     # Send the email
@@ -31,3 +35,16 @@ def send_order_emails(item_name, orders):
     except Exception as e:
         print(f"Error sending email: {e}")
 
+def render_email_content(product_name, orders):
+    # Load the email template
+    with open("email_template.html", "r") as file:
+        template = file.read()
+
+    # Replace placeholders in the template
+    order_details = "\n".join([
+        f"<strong>{order['user_id']}:</strong> {order['quantity']} items" +
+        (" - <em>Assigned for pickup</em>" if order.get("pickup_assigned") else "")
+        for order in orders
+    ])
+    content = template.replace("[Order Name]", product_name)
+    content = content.replace("[Recipient's Name]", "Customer")  # Replace this with
