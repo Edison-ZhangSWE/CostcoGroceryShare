40c1d909f9be996ca126427cdbf6aa3d1a3b7a1a
justusjb <jujo03h@icloud.com>
2023-10-28 23:43:28 -0700

    email handling update

--- backend/email_test.py
+++ backend/email_test.py
@@ -1,17 +1,23 @@
-import os
+email_test.py: import os
 from sendgrid import SendGridAPIClient
 from sendgrid.helpers.mail import Mail
 
 SENDGRID_API_KEY = "API_KEY"
 
 def send_order_emails(item_name, orders):
+    # Convert the list of dictionaries into a list of tuples (email, quantity)
+    order_tuples = [(order['user_id'], order['quantity']) for order in orders]
+
     # Convert the list of tuples into a formatted string for the email body
-    order_details = "\n".join([f"{email}: {quantity}" for email, quantity in orders])
+    order_details = "\n".join([f"{email}: {quantity}" for email, quantity in order_tuples])
+
+    # Create a list of recipient emails from the orders
+    recipient_emails = [order['user_id'] for order in orders]
 
     # Create the email content
     message = Mail(
         from_email="edisonzhang2@gmail.com",
-        to_emails=["edisonzhang10@gmail.com", "edisonzhangsw@gmail.com", "edisonzhang6969@gmail.com"],
+        to_emails=recipient_emails,
         subject=f"Order Confirmation for {item_name}",
         html_content=f"<strong>Order Details:</strong><br>{order_details}"
     )
@@ -23,14 +29,3 @@ def send_order_emails(item_name, orders):
         print(response.status_code, response.body, response.headers)
     except Exception as e:
         print(f"Error sending email: {e}")
-
-# Hardcoded test data
-item_name = "Test Item"
-orders = [
-    ("edisonzhang10@gmail.com", 5),
-    ("edisonzhangsw@gmail.com", 3),
-    ("edisonzhang6969@gmail.com", 2)
-]
-
-# Call the function with the test data
-send_order_emails(item_name, orders)
